7d905e0ae5ff9aa2a0b52ed4b68bde9ee52a8679
tbgrati <tobiassgrati@gmail.com>
2024-08-03 15:32:17 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todo , setTodo] = useState("");
+    const [todoList, setTodoList] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    const handleForm = (e) => {
+        e.preventDefault();
+        setTodoList([...todoList, { todoName: todo }]);
+        setTodo("");
+    };
+
+    const deleteTodo = (deleteValue) => {
+        const restTodoList = [
+            ...todoList.filter((val) => {
+                return val.todoName !== deleteValue;
+            }),
+        ];
+        setTodoList(restTodoList);
+    };
+
+    return (
+    <div className={"bg-gray-200 w-full h-screen flex items-center"}>
+        <div className={"w-[500px] mx-auto text-center bg-white p-5"}>
+            <h1 className={"text-5xl font-bold mb-8"}>Todo list</h1>
+            <form onSubmit={handleForm}>
+                <input
+                    className="border-2 placeholder:text-gray-500 rounded-lg border-black w-full p-5 mb-5 text-black"
+                    type="text"
+                    placeholder="Add Todo"
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                />
+                <button
+                    type="submit"
+                    className="bg-red-600 text-white py-3 px-8 rounded-lg mb-8"
+                >
+                    Add todo
+                </button>
+            </form>
+            <div className={"todo-show-area"}>
+                <ul>
+                    {todoList.map((singleTodo, index) => {
+                        return (
+                            <li
+                                key={index}
+                                className={"bg-black mb-5 flex justify-between  text-white py-5 rounded-lg text-3xl px-5"}
+                            >
+                                {singleTodo.todoName}{" "}
+                                <span
+                                    onClick={() => deleteTodo(singleTodo.todoName)}
+                                    className={"text-red-600 cursor-pointer"}
+                                >
+                                x
+                                </span>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
